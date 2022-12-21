1ab16ac99715f9c03e4605642d94c6aa972848ed
oumaimasandbox <oumaimabendidi15@gmail.com>
2022-12-21 17:58:23 +0100

    updated some

--- src/main/java/com/example/uorders/HelloApplication.java
+++ src/main/java/com/example/uorders/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View/YourOrder.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View/MenudetailsView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("uorders!");
         stage.setScene(scene);
