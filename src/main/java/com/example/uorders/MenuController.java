f22876ca5dfa18dffe18763e12e18d031af33981
HibaQa09 <98495712+HibaQa09@users.noreply.github.com>
2022-12-28 16:47:36 +0100

    hi

---
+++ src/main/java/com/example/uorders/MenuController.java
@@ -0,0 +1,168 @@
+package com.example.uorders;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuController {
+
+    @FXML
+    private Button BeldiButton;
+
+    @FXML
+    private Button BreakfastButton;
+
+    @FXML
+    private ImageView CartButton;
+
+    @FXML
+    private Button DrinksButton;
+
+    @FXML
+    private Button HamburgerMenu;
+
+    @FXML
+    private ImageView PastaButton;
+
+    @FXML
+    private Button PizzaButton;
+
+    @FXML
+    private Button SaladButton;
+
+    @FXML
+    private Button SandwichButton;
+
+    @FXML
+    private Button TacosButton;
+
+    @FXML
+    private Button CartButton0;
+
+    @FXML
+    private Button MenuButton;
+
+    @FXML
+    private AnchorPane MyPanel;
+    private Parent fxml;
+
+
+    @FXML
+    void OpenBeldiMenu(ActionEvent event) throws IOException {
+        MyPanel.getScene().getWindow().hide();
+        Stage stage1 = new Stage();
+        fxml = FXMLLoader.load(getClass().getResource("View/orderView.fxml"));
+        Scene scene = new Scene(fxml);
+        stage1.setScene(scene);
+        stage1.show();
+
+    }
+
+    @FXML
+    void OpenBreakfastMenu(ActionEvent event) throws IOException {
+        MyPanel.getScene().getWindow().hide();
+        Stage stage1 = new Stage();
+        fxml = FXMLLoader.load(getClass().getResource("View/pizzaMenu.fxml"));
+        Scene scene = new Scene(fxml);
+        stage1.setScene(scene);
+        stage1.show();
+
+    }
+
+    @FXML
+    void OpenCart(MouseEvent event)  {
+
+
+    }
+
+    @FXML
+    void OpenDrinksMenu(ActionEvent event) throws IOException {
+        MyPanel.getScene().getWindow().hide();
+        Stage stage1 = new Stage();
+        fxml = FXMLLoader.load(getClass().getResource("View/pizzaMenu.fxml"));
+        Scene scene = new Scene(fxml);
+        stage1.setScene(scene);
+        stage1.show();
+
+    }
+
+    @FXML
+    void OpenPastaMenu(MouseEvent event) throws IOException {
+        MyPanel.getScene().getWindow().hide();
+        Stage stage1 = new Stage();
+        fxml = FXMLLoader.load(getClass().getResource("View/pizzaMenu.fxml"));
+        Scene scene = new Scene(fxml);
+        stage1.setScene(scene);
+        stage1.show();
+
+    }
+
+    @FXML
+    void OpenPizzaMenu(ActionEvent event) throws IOException {
+        MyPanel.getScene().getWindow().hide();
+        Stage stage1 = new Stage();
+        fxml = FXMLLoader.load(getClass().getResource("View/pizzaMenu.fxml"));
+        Scene scene = new Scene(fxml);
+        stage1.setScene(scene);
+        stage1.show();
+
+    }
+
+    @FXML
+    void OpenSaladMenu(ActionEvent event) throws IOException {
+        MyPanel.getScene().getWindow().hide();
+        Stage stage1 = new Stage();
+        fxml = FXMLLoader.load(getClass().getResource("View/pizzaMenu.fxml"));
+        Scene scene = new Scene(fxml);
+        stage1.setScene(scene);
+        stage1.show();
+
+    }
+
+    @FXML
+    void OpenSandwichMenu(ActionEvent event) throws IOException {
+        MyPanel.getScene().getWindow().hide();
+        Stage stage1 = new Stage();
+        fxml = FXMLLoader.load(getClass().getResource("View/pizzaMenu.fxml"));
+        Scene scene = new Scene(fxml);
+        stage1.setScene(scene);
+        stage1.show();
+
+    }
+
+    @FXML
+    void OpenTacosMenu(ActionEvent event) throws IOException {
+        MyPanel.getScene().getWindow().hide();
+        Stage stage1 = new Stage();
+        fxml = FXMLLoader.load(getClass().getResource("View/pizzaMenu.fxml"));
+        Scene scene = new Scene(fxml);
+        stage1.setScene(scene);
+        stage1.show();
+
+    }
+
+    @FXML
+    void Cart(ActionEvent event) throws IOException {
+        MyPanel.getScene().getWindow().hide();
+        Stage stage1 = new Stage();
+        fxml = FXMLLoader.load(getClass().getResource("View/YourOrder.fxml"));
+        Scene scene = new Scene(fxml);
+        stage1.setScene(scene);
+        stage1.show();
+
+    }
+    @FXML
+    void ShowMenu(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/example/uorders/OrderController.java
@@ -0,0 +1,110 @@
+package com.example.uorders;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
+public class OrderController {
+
+    @FXML
+    private Label NbrItems;
+
+    int nbr;
+
+
+    @FXML
+    private Button CommentButton;
+
+    @FXML
+    private TextField CommentsArea;
+
+
+    @FXML
+    private Button CartButton;
+
+    @FXML
+    private Button DecreaseButton;
+
+    @FXML
+    private Button IncreaseButton;
+
+    @FXML
+    private Button OrderButton;
+
+    @FXML
+    private Button ReturnButton;
+
+
+
+    @FXML
+    private AnchorPane MyPanel;
+    private Parent fxml;
+
+
+    @FXML
+    void Decrease(ActionEvent event) {
+        nbr = Integer.parseInt(NbrItems.getText());
+        nbr--;
+        NbrItems.setText(String.valueOf(nbr));
+
+    }
+
+    @FXML
+    void Increase(ActionEvent event) {
+        nbr = Integer.parseInt(NbrItems.getText());
+        nbr++;
+        NbrItems.setText(String.valueOf(nbr));
+
+
+    }
+
+    @FXML
+    void ProceedToConfirmation(ActionEvent event) throws IOException {
+        MyPanel.getScene().getWindow().hide();
+        Stage stage1 = new Stage();
+        fxml = FXMLLoader.load(getClass().getResource("View/YourOrder.fxml"));
+        Scene scene = new Scene(fxml);
+        stage1.setScene(scene);
+        stage1.show();
+
+    }
+
+    @FXML
+    void ShowComment(ActionEvent event) {
+        CommentsArea.setVisible(true);
+        String comnt = CommentsArea.getText();
+
+    }
+    @FXML
+    void Cart(ActionEvent event) throws IOException {
+        MyPanel.getScene().getWindow().hide();
+        Stage stage1 = new Stage();
+        fxml = FXMLLoader.load(getClass().getResource("View/YourOrder.fxml"));
+        Scene scene = new Scene(fxml);
+        stage1.setScene(scene);
+        stage1.show();
+
+    }
+    @FXML
+    void Return(ActionEvent event) throws IOException {
+        MyPanel.getScene().getWindow().hide();
+        Stage stage1 = new Stage();
+        fxml = FXMLLoader.load(getClass().getResource("View/MenuView.fxml"));
+        Scene scene = new Scene(fxml);
+        stage1.setScene(scene);
+        stage1.show();
+
+    }
+
+
+}
