acd3f57a898cb26536a3746fa843861dc1e62e1c
oumaimasandbox <oumaimabendidi15@gmail.com>
2023-01-08 17:47:54 +0100

    Added the Database class,please do not forget to install the jar and add it to your project structures->modules :so you won't have errors in your project

---
+++ src/main/java/com/example/uorders/OrderstatsController.java
@@ -0,0 +1,62 @@
+package com.example.uorders;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class OrderstatsController {
+    @FXML
+    private AnchorPane OrderStatus;
+    private Parent fxml;
+
+    @FXML
+    private ImageView cart;
+
+    @FXML
+    private ImageView menuButton;
+
+    @FXML
+    private RadioButton orderConfButton;
+
+    @FXML
+    private RadioButton orderReady;
+
+    @FXML
+    private RadioButton orderinPrep;
+
+    @FXML
+    private ImageView returnButtton;
+
+
+
+    @FXML
+    void Cart(MouseEvent event) throws IOException  {
+        OrderStatus.getScene().getWindow().hide();
+        Stage stage1 = new Stage();
+        fxml = FXMLLoader.load(getClass().getResource("View/YourOrder.fxml"));
+        Scene scene = new Scene(fxml);
+        stage1.setScene(scene);
+        stage1.show();
+
+    }
+
+
+
+    @FXML
+    void Return(MouseEvent event)  throws IOException {
+        OrderStatus.getScene().getWindow().hide();
+        Stage stage1 = new Stage();
+        fxml = FXMLLoader.load(getClass().getResource("View/MenuView.fxml"));
+        Scene scene = new Scene(fxml);
+        stage1.setScene(scene);
+        stage1.show();
+    }
+}
