6965d61fba37daaf3f69297cd24ccd042cfb2d6a
HibaQa09 <98495712+HibaQa09@users.noreply.github.com>
2023-01-19 10:30:42 +0100

    menu category items

---
+++ src/main/java/com/example/uorders/MenuCatController.java
@@ -0,0 +1,26 @@
+
+package com.example.uorders;
+
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.Label;
+        import javafx.scene.image.ImageView;
+
+public class MenuCatController {
+
+    @FXML
+    private Label Cat_name;
+
+    @FXML
+    private Button CategoryButton;
+
+    @FXML
+    private ImageView img;
+
+    @FXML
+    void OpenItemMenu(ActionEvent event) {
+
+    }
+
+}
