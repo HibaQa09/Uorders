2085513d36c541fe33c26198c17d1ee9856bf9c7
HibaQa09 <98495712+HibaQa09@users.noreply.github.com>
2023-01-21 18:46:31 +0100

    menu category items

---
+++ src/main/java/com/example/models/CategoryModel.java
@@ -0,0 +1,25 @@
+package com.example.models;
+
+public class CategoryModel {
+    private String name;
+    private String imgSrc;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgSrc() {
+        return imgSrc;
+    }
+
+    public void setImgSrc(String imgSrc) {
+        this.imgSrc = imgSrc;
+    }
+
+
+
+}
--- src/main/java/com/example/uorders/MenuCatController.java
+++ src/main/java/com/example/uorders/MenuCatController.java
@@ -1,11 +1,15 @@
 
 package com.example.uorders;
 
+        import com.example.models.CategoryModel;
         import javafx.event.ActionEvent;
         import javafx.fxml.FXML;
         import javafx.scene.control.Button;
         import javafx.scene.control.Label;
+        import javafx.scene.image.Image;
         import javafx.scene.image.ImageView;
+
+        import java.util.Objects;
 
 public class MenuCatController {
 
@@ -17,6 +21,16 @@ public class MenuCatController {
 
     @FXML
     private ImageView img;
+
+    private CategoryModel category;
+
+    public void setData(CategoryModel category){
+        this.category=category;
+        Cat_name.setText(category.getName());
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream(category.getImgSrc())));
+        img.setImage(image);
+
+    }
 
     @FXML
     void OpenItemMenu(ActionEvent event) {
