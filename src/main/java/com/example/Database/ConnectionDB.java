1b2921c73de822e9b92a1253880966df8f864d32
oumaimasandbox <oumaimabendidi15@gmail.com>
2023-01-27 20:32:18 +0100

    edited some dabase

--- src/main/java/com/example/Database/ConnectionDB.java
+++ src/main/java/com/example/Database/ConnectionDB.java
@@ -5,33 +5,26 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class ConnectionDB {
-    pubpackage com.example.Database;
-import java.sql.Connection;
+    public static Connection getConnexion() {
+        Connection con = null;
+        try {
+            // 1 Charger le driver mysql
+            Class.forName("com.mysql.jdbc.Driver");
 
-import java.sql.DriverManager;
-import java.sql.Statement;
+            // 2 Créer la connexion
+            con = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3301/UordersDB" , "root","");
+            System.out.println("Connection is created successfully:");
 
-    public class ConnectionDB {
-        public static Connection getConnexion() {
-            Connection con = null;
-            try {
-                // 1 Charger le driver mysql
-                Class.forName("com.mysql.jdbc.Driver");
+            //3 Créer un état de connexion
+            Statement st = ((java.sql.Connection) con).createStatement();
 
-                // 2 Créer la connexion
-                con = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3301/UordersDB" , "root","");
-                System.out.println("Connection is created successfully:");
-
-                //3 Créer un état de connexion
-                Statement st = ((java.sql.Connection) con).createStatement();
-
-            }catch(Exception e) {
-                System.out.println("Error: "+e.getMessage());
-                e.printStackTrace();
-            }
-            return con;
-
+        }catch(Exception e) {
+            System.out.println("Error: "+e.getMessage());
+            e.printStackTrace();
         }
+        return con;
+//lol
+    }
 
     public static void main(String[] args) {
         getConnexion();
